77066b3a215fbdb4fe7aef8ef7b183c2d72c143b
Michael <mcheng.oc@gmail.com>
2020-10-25 12:15:40 -0700

    style(donate-section-one): styles component

--- src/sections/DonateSectionOne/DonateSectionOne.js
+++ src/sections/DonateSectionOne/DonateSectionOne.js
@@ -1,18 +1,28 @@
 import React from 'react';
+import createDOMPurify from 'dompurify';
+import { JSDOM } from 'jsdom';
 
 import Button from 'components/Button/Button';
 import './DonateSectionOne.scss';
 
 const DonateSectionOne = ({ data }) => {
-  console.log(data);
+  const window = (new JSDOM('')).window;
+  const DOMPurify = createDOMPurify(window);
+  const cleanText = DOMPurify.sanitize(data.text);
+  
+  function createTextMarkup() {
+    return {__html: cleanText};
+  }
+
   return (
     <div className='donate-section-one'>
-      <h1>{data.title}</h1>
-      <div>
-        <div>
-          <p>{data.text}</p>
-          <Button link={data.button_link}>{data.button_text}</Button>
+      <h1 className='donate-section-one__title'>{data.title}</h1>
+      <div className='donate-section-one__body'>
+        <div className='donate-section-one__content'>
+          <div className='donate-section-one__text' dangerouslySetInnerHTML={createTextMarkup()} />
+          <Button link={data.button_link} type='filled'>{data.button_text}</Button>
         </div>
+        <img className='donate-section-one__image' src={data.image_link} alt='Giving To HUOA'/>
       </div>
     </div>
   )
